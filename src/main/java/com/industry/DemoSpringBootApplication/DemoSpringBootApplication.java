3a737c1aa782bcb81eea38831dc23f6e87ed3295
Rahul Singh <rahulsinghmail1@gmail.com>
2022-11-17 13:17:49 +0530

    newcommit

--- src/main/java/com/industry/DemoSpringBootApplication/DemoSpringBootApplication.java
+++ src/main/java/com/industry/DemoSpringBootApplication/DemoSpringBootApplication.java
@@ -1,16 +1,4 @@
-package com.industry.DemoSpringBootApplication;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DemoSpringBootApplication {
-
-	public static void main(String[] args) {
-
-		dfsdf.run(DemoSpringBootApplication.class, args);
-Rahul Singh
-
-	}
-
-}
+master hai ye wala
+123455
+1232432423
+2423432
