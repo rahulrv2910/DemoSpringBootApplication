b4c162ad90a42ee4ca97314dd056246b4019eabb
Rahul Singh <rahulsinghmail1@gmail.com>
2022-11-17 13:11:00 +0530

    new3

--- src/main/java/com/industry/DemoSpringBootApplication/DemoSpringBootApplication.java
+++ src/main/java/com/industry/DemoSpringBootApplication/DemoSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringBootApplication.class, args);
+		dfsdf.run(DemoSpringBootApplication.class, args);
 Rahul Singh
 	}
 
